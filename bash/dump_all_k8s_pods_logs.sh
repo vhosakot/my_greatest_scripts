87945cac1876bff4601f83408bb6686b5ececa2b
Vikram Hosakote <vhosakot@cisco.com>
2019-06-17 16:41:12 -0400

    Update dump_all_k8s_pods_logs.sh

--- bash/dump_all_k8s_pods_logs.sh
+++ bash/dump_all_k8s_pods_logs.sh
@@ -8,3 +8,10 @@ kubectl get pods | grep Run | awk '{print $1}' | xargs -I % sh -c "
   echo ============ kubectl logs % >> all_pods_logs &&
   kubectl logs % >> all_pods_logs &&
   echo >> all_pods_logs"
+  
+# get logs of all containers in all the istio pods in istio-system namespace
+
+kubectl get pods -n=istio-system | awk '{print $1}' | grep -v '^NAME' | xargs -I % sh -c "
+    echo ======== kubectl logs % -n=istio-system --all-containers=true ======== >> all_istio_pods_logs.txt &&
+    kubectl logs % -n=istio-system --all-containers=true >> all_istio_pods_logs.txt &&
+    echo >> all_istio_pods_logs.txt"
